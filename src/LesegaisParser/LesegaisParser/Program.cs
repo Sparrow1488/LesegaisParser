fc4431842a8267eb64a191e77f4bdd6bacd3cf96
Sparrow1488 <ilyaokubev@gmail.com>
2021-11-23 18:18:18 +0300

    LP: редактирование

--- src/LesegaisParser/LesegaisParser/Program.cs
+++ src/LesegaisParser/LesegaisParser/Program.cs
@@ -1,5 +1,8 @@
-﻿using Kernel.Tools.Logging;
+﻿using Kernel.Layer;
+using Kernel.Tools.Logging;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace LesegaisParser
@@ -12,7 +15,7 @@ namespace LesegaisParser
         {
             while (true)
             {
-                Parser.ParseItemsCount = 200;
+                Parser.ParseItemsCount = 500;   // в каждом запросе мы будем вытягивать по 500 договоров
                 var total = Parser.GetTotalSizeAsync().GetAwaiter().GetResult();
                 ParseAbsolutelyAllData(total);
 
@@ -27,20 +30,23 @@ namespace LesegaisParser
         private static void ParseAbsolutelyAllData(int totalSize)
         {
             var requestsCount = totalSize / Parser.ParseItemsCount;
-            var lastRequestCount = totalSize % Parser.ParseItemsCount;    // последний запрос - исключение
+            var lastRequestCount = totalSize % Parser.ParseItemsCount;
 
             for (int i = 0; i < requestsCount; i++)
             {
                 var deals = Parser.ParseAsync().GetAwaiter().GetResult();
                 if (deals.Length > 0)
+                {
                     Logger.Success($"Сделки успешно получены ({i + 1}/{requestsCount + 2})");
+                    AddInDb(deals);
+                }
                 else Logger.Error($"Возникла ошибка в ходе получения сделок, либо же в ответ ничего не пришло ({i + 1}/{requestsCount + 2})");
-               
+
                 Parser.ParseCurrentPage++;
             }
 
             // последний запрос
-            Parser.ParseCurrentPage = requestsCount + 1;
+            Parser.ParseCurrentPage = requestsCount;
             Parser.ParseItemsCount = lastRequestCount;
             var lastDeals = Parser.ParseAsync().GetAwaiter().GetResult();
             if (lastDeals.Length > 0)
@@ -48,5 +54,14 @@ namespace LesegaisParser
             else
                 Logger.Error($"Возникла ошибка в ходе получения ответа ({requestsCount + 1}/{requestsCount + 1})");
         }
+
+        private static void AddInDb(IEnumerable<Woodreportdeals> deals)
+        {
+            foreach (var deal in deals)
+            {
+                var result = deal.Save(); // ошибка: не инициализирован connectionString
+            }
+        }
+        
     }
 }
